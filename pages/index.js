961f1a747d93233dca8a153d1b8b3b297bcf1b18
enieber cunha <eniebercunha@gmail.com>
2022-06-18 19:38:02 -0300

    tag: first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Boas vindas ao projeto Falou</h1>
+      <p>
+        O projeto Falou tem como objetivo mapear te organizar tudo que os
+        senadores do Brasil falaram durante seu mandato
+      </p>
+    </div>
+  );
+}
